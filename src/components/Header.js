c92df4dd0cef0fe1c9b5f6d3868c5b3adafe078e
Ahsan-Rahim <Ahsan.Rahim@ibm.com>
2020-01-02 16:13:57 -0600

    fixing mixed repos

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,6 @@ const Header = () => (
         <h1>Expensify</h1>
         <NavLink to="/" activeClassName="is-active" exact>Dashboard</NavLink>
         <NavLink to="/create" activeClassName="is-active" exact>Create Expense</NavLink>
-        <NavLink to="/edit" activeClassName="is-active" exact>Edit Expense</NavLink>
         <NavLink to="/help" activeClassName="is-active" exact>Help </NavLink>
     </header>
 );
